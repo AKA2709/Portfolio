fabf19f146e8e493feb992f5c6ce7f402b546bb4
AKA2709 <amritkalash9523@gmail.com>
2025-10-18 19:06:19 +0530

    firstchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Info from "./components/Info";
 function App() {
   return (
     <div>
-      <Header />
 	<Info />
       <Projects />
 
